6e57615a306c8394c8c029eb4ef7a4a82a46539e
Makan Singh <singh.makan0205@gmail.com>
2023-02-03 16:05:27 +0530

    added registration & login form

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, FormBuilder, Validators, AbstractControl } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { User } from 'src/app/models/auth.model';
 import { AuthServiceService } from 'src/app/services/auth-service.service';
 
@@ -10,6 +11,8 @@ import { AuthServiceService } from 'src/app/services/auth-service.service';
 })
 export class LoginComponent implements OnInit {
 
+  loading:boolean = false;
+  returnUrl: string = '/';
   loginForm: FormGroup = new FormGroup({
     email: new FormControl(''),
     password: new FormControl('')
@@ -17,13 +20,16 @@ export class LoginComponent implements OnInit {
   submitted:boolean = false;
 
   constructor(private fb: FormBuilder,
-    private authService: AuthServiceService){
+    private authService: AuthServiceService,
+    private router: Router,
+    private route: ActivatedRoute){
   }
   ngOnInit(): void {
     this.loginForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
+      email: ['', [Validators.required]],
       password: ['', [Validators.required]]
     })
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
   }
 
   get form(): { [key: string]: AbstractControl } {
@@ -32,10 +38,19 @@ export class LoginComponent implements OnInit {
 
   onSubmit(){
     this.submitted = true
-    console.log(this.loginForm, 'reactiveform')
-    let userDetails = this.loginForm.value
-    this.authService.login(userDetails).subscribe((res) => {
-      console.log(res,'res')
-    })
+      
+    if (this.loginForm.valid) {
+      this.loading = true;
+      let userDetails = this.loginForm.value
+      this.authService.login(userDetails).subscribe(
+        {
+          next: (data: any) => this.router.navigate([this.returnUrl]),
+          error: (err: { message: any; }) => {
+            alert(err.message)
+            this.loading = false;
+          },
+          complete: () => console.info('Logged In')
+        });
+    }
   }
 }
