bef46c9b2ba0dc2fa2675f6f8ab06919a5f86650
Makan Singh <singh.makan0205@gmail.com>
2023-02-04 16:24:42 +0530

    create post integration done

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Posts } from 'src/app/_model/posts.model';
+import { Users } from 'src/app/_model/users.model';
 import { PostsService } from 'src/app/_services/posts.service';
+import { UserService } from 'src/app/_services/user.service';
 
 @Component({
   selector: 'app-home',
@@ -11,23 +14,58 @@ import { PostsService } from 'src/app/_services/posts.service';
 export class HomeComponent implements OnInit {
 
   allPosts: Posts[] = [];
+  postForm: FormGroup = new FormGroup({
+    post: new FormControl('')
+  });
+  submitted:boolean = false;
+  userInfo:Users={
+    isAdmin: false,
+    isActive: false,
+    _id: '',
+    firstName: '',
+    lastName: '',
+    email: '',
+    dob: '',
+    gender: '',
+    photoId: '',
+    createdDate: '',
+    token: ''
+  }
 
-  constructor(private postsService: PostsService, private router: Router ) { }
+  constructor(private postsService: PostsService,
+    private userService: UserService,
+    private fb: FormBuilder, ) { }
 
   ngOnInit(): void {
     this.postsService.fetchAllPosts().subscribe((response)=>{
-      console.log(response, 'posss')
       this.allPosts = response
     })
+    this.postForm = this.fb.group({
+      post: ['', [Validators.required]]
+    })
+
+    let user = this.userService.retrieveUser();
+    this.userInfo = JSON.parse(user || '')
+  }
+  onSubmit(){
+    this.submitted = true
+      
+    if (this.postForm.valid) {
+     
+      let postData ={
+        ...this.userInfo,
+        ...this.postForm.value
+      } 
+      
+      this.postsService.createAPosts(postData).subscribe(
+        {
+          next: (data: any) => {this.postForm.reset()},
+          error: (err: { message: any; }) => {
+            alert(err.message)
+          },
+          complete: () => console.info('Post Created')
+        });
+    }
   }
 
-  // goToViewBusSeats(busId: number){
-  //   this.router.navigate(['bus-list', busId]);
-  // }
-
-  // searchBus(eventData: { leavingFrom: number, goingTo: number, departureDate: string}) {
-  //   this.busService.searchBus(eventData.leavingFrom, eventData.goingTo).subscribe((response)=>{
-  //     this.allBus = response;
-  //   })
-  // }
 }
